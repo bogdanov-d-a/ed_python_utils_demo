a38690a0048168dc743b5ece46bd2a54c99f2b41
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2022-05-11 01:23:13 +0300

    Fix file_deduplication/main.py

--- file_deduplication/main.py
+++ file_deduplication/main.py
@@ -1,3 +1,3 @@
 from edpu.file_deduplication import main
 
-main.backup_and_recover('source', 'target', 'bundles', 'data_index.txt', True, False)
+main.backup_and_recover('source', 'target', 'bundles', 'file_index.txt', 'dir_index.txt', True, True, False)
